44ab0a47ce10b8116c68d218125ebe5c1640d7ec
Darnell Baird <drsbaird@yahoo.com>
2023-01-17 14:40:04 -0400

    test: Rename file and add Unity framework template

---
+++ arduino_motorcycle_tools/test/embedded/prioritizer/test_prioritizer.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <unity.h>
+
+void setUp(void){}
+
+void tearDown(void){}
+
+void test_some_embedded_function(void)
+{
+    // TEST_ASSERT_EQUAL(,);
+}
+
+void setup()
+{
+    /* 
+    IDK what the following means.
+    NOTE!!! Wait for >2 secs
+    if board doesn't support software reset via Serial.DTR/RTS 
+    */
+    delay(2000);
+
+    //    Do things for setup here
+
+    // Start unit testing
+    UNITY_BEGIN();
+}
+
+void loop(){
+    // Any further configs?
+
+    // Run tests
+    RUN_TEST(test_some_embedded_function);
+
+    // End unit testing
+    UNITY_END();
+}
